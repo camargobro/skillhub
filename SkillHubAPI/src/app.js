b7e32dd46579711f780a97135c42daef837d56be
Gustavo Camargo <gu_camargo06@hotmail.com>
2025-07-25 23:33:02 -0300

    Conectando a API ao MongoDB

---
+++ SkillHubAPI/src/app.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import conectaDB from './config/dbConnect.js'
+
+const conexao = await conectaDB()
+conexao.on("error", (erro) => {
+    console.error("Erro de conexão", erro)
+})
+
+conexao.once("open", () => {
+    console.log("Conexão com o DB feita com sucesso!")
+})
+
+const app = express()
+const port = 8080
+
+import routeUsuarios from '../routes/routeUsuarios.js'
+
+app.use('/usuarios', routeUsuarios)
+
+
+app.listen(port, () =>{
+    console.log(`Servidor rodando na porta ${port}`)
+})
