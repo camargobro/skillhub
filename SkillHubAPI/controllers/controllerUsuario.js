422206b15d53437756129e860c945d71be7685ea
Gustavo Camargo <gu_camargo06@hotmail.com>
2025-07-29 23:33:10 -0300

    Ajustando controller e model de usuários

--- SkillHubAPI/controllers/controllerUsuario.js
+++ SkillHubAPI/controllers/controllerUsuario.js
@@ -1,35 +1,58 @@
-import Usuario from "../models/modelUsuarios.js"
+import { getUsuarios, deleteUsuario, postUsuario, putUsuario } from "../models/modelUsuarios.js"
 import bcrypt from "bcrypt"
 
 export async function criarUsuario(req, res){
+    try{
+    const {nome, idade, email, senha} = req.body
     const saltRounds = 10;
-    const senhaHash = await bcrypt.hash(req.body.senha, saltRounds)
-    const usuario = new Usuario({
-        nome: req.body.nome,
-        idade: req.body.idade,
-        email: req.body.email,
+    const senhaHash = await bcrypt.hash(senha, saltRounds)
+    const usuario = await postUsuario(
+        {
+        nome: nome,
+        idade: idade,
+        email: email,
         senha: senhaHash
-})
-
-    await usuario.save()
+        }
+    )
     res.status(201).send(usuario)
+    }catch(error){
+    res.status(500).send(error)
+    }
 }
+
 export async function buscarUsuarios(req, res){
-    const usuarios = await Usuario.find()
-    res.status(200).send(usuarios)
+    try{
+        const usuarios = await getUsuarios()
+        console.log(usuarios)
+        res.status(200).send(usuarios)
+    } catch(error){
+        res.status(500).send(error)
+    }
+    
 }
 
 export async function excluirUsuario(req, res){
-    const usuario = await Usuario.findByIdAndDelete(req.params.id)
-    res.status(200).send('Usuário excluído com sucesso: ' + usuario)
+    try{
+    const usuario = await deleteUsuario(req.params.id)
+        res.status(200).send(`Usuário excluído com sucesso: ${usuario}`)
+    } catch(error){
+        res.status(500).send(error)
+    }
 }
 
 export async function editarUsuario(req, res){
-    const usuario = await Usuario.findByIdAndUpdate(req.params.id, {
-        nome: req.body.nome,
-        idade: req.body.idade,
-        email: req.body.email,
-        senha: req.body.senha
+    try{
+    const {nome, idade, email, senha} = req.body
+    const saltRounds = 10;
+    const senhaHash = await bcrypt.hash(senha, saltRounds)
+    const usuario = await putUsuario(req.params.id, {
+        nome: nome,
+        idade: idade,
+        email: email,
+        senha: senhaHash
     })
-    res.status(200).send('Usuário editado com sucesso: ' + usuario)
+    res.status(200).send(`Usuário editado com sucesso: ${usuario}`)
+    } catch(error){
+        res.status(500).send(error)
+    }
 }
--- SkillHubAPI/models/modelUsuarios.js
+++ SkillHubAPI/models/modelUsuarios.js
@@ -1,15 +1,33 @@
-import mongoose, { mongo }  from 'mongoose' 
+import mongoose, { mongo } from 'mongoose'
 
-const usuarioSchema = new mongoose.Schema({ 
-    nome: {type: String, required: true},
-    idade: {type: Number, required: true},
-    email: {type: String, required: true},
-    senha: {type: String, required: true},
-    criadoEm: {type: Date, default: Date.now }
+const usuarioSchema = new mongoose.Schema({
+    nome: { type: String, required: true },
+    idade: { type: Number, required: true },
+    email: { type: String, required: true },
+    senha: { type: String, required: true },
+    criadoEm: { type: Date, default: Date.now }
 })
 
 const Usuario = mongoose.model('usuarios', usuarioSchema);
 
-export default Usuario
+export async function postUsuario(dadosUsuario){
+    const usuario = new Usuario(dadosUsuario)
+    return await usuario.save()
+}
+
+export async function getUsuarios(){
+    return await Usuario.find()
+}
+
+export async function deleteUsuario(id){
+    return await Usuario.findByIdAndDelete(id)
+}
+
+export async function putUsuario(id, usuarioAtualizado){
+    return await Usuario.findByIdAndUpdate(id, usuarioAtualizado)
+}
+
+
+
 
 
